7bdc8ebee3495fd68d723f7a281ab7329247754e
JCY <jcy8206@gmail.com>
2022-08-02 00:26:49 +0900

    JDBC 개발 - 수정, 삭제

--- src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +18,7 @@ class MemberRepositoryV0Test {
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV101", 10000);
         repository.save(member);
 
         Member findMember = repository.findById(member.getMemberId());
@@ -25,6 +26,16 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+
     }
 
 }
